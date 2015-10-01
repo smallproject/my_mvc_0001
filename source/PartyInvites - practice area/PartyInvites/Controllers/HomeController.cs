f4cca297c92e4eb4619215d6edb3592fcaf8dd94
smallproject <j.a.villas@live.com.ph>
2015-10-01 17:04:38 +0200

    minor

---
+++ source/PartyInvites - practice area/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO : Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ViewResult PersonForm()
+        {
+            return View();
+        }
+
+
+        public ViewResult PersonForm(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("MadePerson", person);
+            }
+            return View();
+        }
+    }
+}
---
+++ source/PartyInvites - practice area/PartyInvites/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        public string firstname { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string lastname { get; set; }
+
+        [Required(ErrorMessage = "Gender must be selected.")]
+        public bool? gender { get; set; }
+
+        [Required(ErrorMessage = "Birthdate must be filled.")]
+        public DateTime birthdate { get; set; }
+    }
+}
