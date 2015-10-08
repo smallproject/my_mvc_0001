119e02a5f29391dbd4ff464c1bfbe570b31418bc
smallproject <j.a.villas@live.com.ph>
2015-10-08 16:24:32 +0200

    minor

---
+++ source/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            webfunction1();
+        }
+
+        static void webfunction()
+        {
+            Uri uri = new Uri("http://www.google.com/");
+
+            Console.WriteLine(uri.AbsolutePath);
+            Console.WriteLine(uri.AbsoluteUri);
+            Console.WriteLine(uri.Authority);
+            Console.WriteLine(uri.DnsSafeHost);
+            Console.WriteLine(uri.Fragment);
+            Console.WriteLine(uri.Host);
+            Console.WriteLine(uri.HostNameType);
+            Console.WriteLine(uri.IsAbsoluteUri);
+            Console.WriteLine(uri.IsDefaultPort);
+            Console.WriteLine(uri.IsFile);
+            Console.WriteLine(uri.IsLoopback);
+            Console.WriteLine(uri.IsUnc);
+            Console.WriteLine(uri.LocalPath);
+            Console.WriteLine(uri.OriginalString);
+            Console.WriteLine(uri.PathAndQuery);
+            Console.WriteLine(uri.Port);
+            Console.WriteLine(uri.Query);
+            Console.WriteLine(uri.Scheme);
+            Console.WriteLine(uri.Segments);
+            Console.WriteLine(uri.UserEscaped);
+            Console.WriteLine(uri.UserInfo);
+        }
+
+        static void webfunction1()
+        {
+            string urlAddress = "http://google.com";
+
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ source/PartyInvites - practice area/PartyInvites/Scripts/JavaScript1.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $(this).animate({
+            height: '+=10px'
+        });
+    });
+
+    $('div').mouseleave(function() {
+        $(this).animate({
+            height: '-=10px'
+        });
+    });
+
+    $().mouseenter();
+    $().mouseleave();
+
+    $("#red").mouseenter(function () {
+        $(this).fadeOut(1000);
+    });
+    $("#red").mouseleave(function() {
+        $(this).fadeIn(1000);
+    });
+
+    $("#blue").mouseenter(function() {
+        $(this).slideUp("slow");
+    });
+    $("#blue").mouseleave(function() {
+        $(this).slideDown("slow");
+    });
+});
