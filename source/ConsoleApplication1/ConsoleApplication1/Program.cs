c81a976d38008c88b8104500855281f2d7c3eb5b
smallproject <j.a.villas@live.com.ph>
2015-10-11 07:28:25 +0200

    minor

--- source/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ source/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            webfunction1();
+            PrintHelpPage();
         }
 
         static void webfunction()
@@ -42,6 +46,8 @@ namespace ConsoleApplication1
             Console.WriteLine(uri.UserInfo);
         }
 
+
+        
         static void webfunction1()
         {
             string urlAddress = "http://google.com";
@@ -69,6 +75,85 @@ namespace ConsoleApplication1
                 readStream.Close();
 
                 Console.WriteLine(data);
+            }
+        }
+
+        private static bool completed = false;
+        private static WebBrowser webBrowserForPrinting = new WebBrowser();
+        static void PrintHelpPage()
+        {
+
+            webBrowserForPrinting.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(PrintDocument);
+
+            //webBrowserForPrinting.Url = new Uri(@"www.google.com");
+            //webBrowserForPrinting.Navigate(@"www.google.com");
+            webBrowserForPrinting.Navigate(@"C:\git_root\my_mvc_0001\source\ConsoleApplication1\ConsoleApplication1\Website\HTMLPage1.html");
+
+            while (!completed)
+            {
+                Application.DoEvents();
+                Thread.Sleep(100);
+            }
+
+            Console.Write("\n\nDone with it!\n\n");
+            Console.ReadLine();
+        }
+
+        static void PrintDocument(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //((webbrowser)sender).print();
+
+            //((webbrowser)sender).dispose();
+            
+            //Console.WriteLine(webBrowserForPrinting.Document.Body.InnerHtml);
+
+            //DisplayMetaDescription();
+            TestHtml();
+            completed = true;
+        }
+
+        static void DisplayMetaDescription()
+        {
+            if (webBrowserForPrinting.Document != null)
+            {
+                HtmlElementCollection elems = webBrowserForPrinting.Document.GetElementsByTagName("meta");
+                foreach (HtmlElement htmlElement in elems)
+                {
+                    //string nameStr = htmlElement.GetAttribute("name");
+                    string nameStr = htmlElement.GetAttribute("charset");
+                    
+                    Console.WriteLine(htmlElement.All);
+                    Console.WriteLine(nameStr);
+                    //if (nameStr != null && nameStr.Length != 0)
+                    //{
+                    //    string contentStr = htmlElement.GetAttribute("content");
+                    //    Console.WriteLine("Document: " + webBrowserForPrinting.Url.ToString() + "\nDescription: " + contentStr);
+                    //}
+                }
+            }
+        }
+
+        static void TestHtml()
+        {
+            if (webBrowserForPrinting.Document != null)
+            {
+                HtmlElementCollection elems = webBrowserForPrinting.Document.GetElementsByTagName("div");
+                foreach (HtmlElement htmlElement in elems)
+                {
+                    Console.WriteLine(htmlElement.InnerText);
+                    //foreach (HtmlElement element in htmlElement)
+                    //{
+                        
+                    //}
+
+                    Console.WriteLine("\n\n\n\n");
+                    //Console.WriteLine(htmlElement.InnerHtml);
+
+                    //if (htmlElement.GetAttribute("classname").ToString().Equals("g"))
+                    //{
+                    //    Console.WriteLine(htmlElement.GetAttribute("InnerText"));
+                    //}
+                }
             }
         }
     }
